753992d9774e450d0e4a5d6b1001e37ba8508229
siegerrs <siegerrs@rose-hulman.edu>
2024-02-02 16:46:55 -0500

    hero diagonal movement

--- src/mainApp/Hero.java
+++ src/mainApp/Hero.java
@@ -8,57 +8,80 @@ import java.awt.event.KeyEvent;
 
 
 public class Hero {
+	
     private int x;
     private int y;
     private int moveX;
     private int moveY;
     private boolean jumpKeyPressed = false;
+    private boolean isJumping = false;
+    private boolean isGoingUp = false; // Flag to track the direction of diagonal movement
 
     public Hero(int x, int y, int changeX) {
         this.x = x;
         this.y = y;
         this.moveX = changeX;
+//        this.moveY = 0; // Initialize the vertical movement to zero
     }
+
     public int getX() {
-    	return this.x;
- 
+        return this.x;
     }
-    
+
     public int getY() {
-    	return this.y;
+        return this.y;
     }
 
     public void move() {
+        if (jumpKeyPressed && !isJumping) {
+            // If space bar is pressed and not already jumping, initiate diagonal jump
+            isJumping = true;
+            isGoingUp = true; // Set the flag for upward diagonal movement
+        }
+
+        // Diagonal movement
         this.x += this.moveX;
+
+        // Handle vertical movement during diagonal jump
+        if (isJumping) {
+            if (isGoingUp) {
+                this.moveY = -1; // Move upwards during diagonal jump
+            } else {
+                this.moveY = 1; // Move downwards during diagonal jump
+            }
+        } else {
+            this.moveY = 1; // Automatically fall diagonally when not jumping
+        }
+
+        this.y += this.moveY;
+
+        // Limiting x coordinate
         if (this.x > 900) {
             this.x = 900;
         }
-        this.y += this.moveX;
+
+        // Limiting y coordinate
         if (this.y < 0) {
-        	this.y = 0;
+            this.y = 0;
+            isJumping = false; // Reset jumping status when reaching the top
         }
         if (this.y > 475) {
-        	this.y = 475;
+            this.y = 475;
+            isJumping = false; // Reset jumping status when reaching the bottom
         }
-        
-      
- //           this.moveX = -this.moveX; // Reverse direction when hitting left edge
-        }
-//        } else if (x > 1000) { // Assuming width of the screen is 400
-//            this.x = 1000;
-//            this.moveX = -this.moveX; // Reverse direction when hitting right edge
-//        }
-    
+    }
 
     public void setYPosition(int changeY) {
-    	this.y += changeY;
-    	if(this.y < 0) {
-    		this.y = 0;
-    		
-    	}
+        this.y += changeY;
+        if (this.y < 0) {
+            this.y = 0;
+        }
     }
-    
-    
+
+    // Call this method when space bar is pressed to initiate the diagonal jump
+    public void startJump() {
+        jumpKeyPressed = true;
+    }
 }
 
 
