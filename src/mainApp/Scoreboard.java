7bf6f9e5c6fb2390c8a04d433080b0ded33c09ca
shaffemm <shaffemm@rose-hulman.edu>
2024-02-05 16:16:27 -0500

    Started scoreboard

--- src/mainApp/Scoreboard.java
+++ src/mainApp/Scoreboard.java
@@ -3,17 +3,25 @@ package mainApp;
 public class Scoreboard {
 
 	private Hero hero;
+	private int committedCoins;
 	
 	public Scoreboard(Hero hero) {
 		this.hero = hero;
 	}
 	
-	public int countCoins(){
-		return this.hero.getCoinCount();
+	//add coins collected to permanent score (should happen at end of level)
+	public void commitCoins() {
+		committedCoins += this.hero.getCoinCount();
 	}
 	
-	public int countlives(){
-		return this.hero.getLives(); 
+	//return count of TOTAL CURRENT COINS, committed + non committed
+	public int countCoins(){
+		return this.committedCoins + this.hero.getCoinCount();
+	}
+	
+	//return count of current lives
+	public int countlives(Hero hero){
+		return hero.getLives(); 
 	}
 	
 	public static void endGame(){
